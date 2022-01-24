5e619f61f3b8bebeb2ec2139462ef23f784a1d4d
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro.local>
2022-01-25 04:21:49 +0630

    update

--- init/init.js
+++ init/init.js
@@ -5,8 +5,12 @@ const BASE_URL = process.env.PYOPENAI;
 
 exports.initApp = async () => {
   const known_faces = await KnownFace.find({}, "url label -_id");
-  axios
-    .post(`${BASE_URL}/face-detector/load-known-faces`, { known_faces })
-    .then((res) => console.log(res.data.message))
-    .catch(console.log);
+  setTimeout(() => {
+    axios
+      .post(`${BASE_URL}/face-detector/load-known-faces`, { known_faces })
+      .then((res) => {
+        console.log(res.data.message);
+      })
+      .catch(console.log);
+  }, 5000);
 };
